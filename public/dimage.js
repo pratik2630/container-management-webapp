94b3b5fba813e885cceee2efde66de626fb7df9e
rohit <rohitrambade18@gmail.com>
2022-08-14 23:34:27 +0530

    late night changes

--- public/dimage.js
+++ public/dimage.js
@@ -5,7 +5,8 @@ function dimage_code(req, res) {
      exec("docker images | tail -n +2", (err, stdout, stderr) => {
      
         let a = stdout.split("\n");
-        res.write("<table  align='center' width='80%'>");
+        res.write("<table >"); 
+        // align='center' width='80%'
        
         a.forEach((cdetails) => {
             const cinfo = cdetails.trim().split(/\s+/)
