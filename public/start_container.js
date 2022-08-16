af4ca41256af323ec2e31365b57e7bfde9b33c8e
Rohit Rambade <86614477+rohit-rambade@users.noreply.github.com>
2022-08-16 19:19:49 +0530

    Update start_container.js

--- public/start_container.js
+++ public/start_container.js
@@ -3,11 +3,12 @@ const express = require("express")
 const { exec } = require("child_process");
 
 function start_container(req, res) {
-
+    
     var c_name=req.query.container_name;
 	
     cname = "docker start " + c_name ;
-    
+    console.log("c_name:"+c_name)
+    console.log("cname:"+cname)
 
 	exec(cname , (err , stdout , stderr)=>{
 		res.write("<br /><pre>"+stdout+"</pre>");
