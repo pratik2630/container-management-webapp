738083d738aa0ba4b40e054f4dd8903e965d059c
pratik2630 <pratikkorgaonkar26@gmail.com>
2022-08-19 22:28:59 +0530

    Added dockerfile for basic nodejs project

---
+++ devops/docker/nodejs_project/server.js
@@ -0,0 +1,14 @@
+const express = require("express"); //imported express js
+
+//Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+//App
+const app = express();
+app.get("/",(req,res) =>{
+	res.send("Hello world");
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
